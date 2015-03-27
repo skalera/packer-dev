898763964e05af040f924f85ee6cb15ba1b3a94f
Martin Englund <martin@englund.nu>
2015-03-27 12:53:21 -0700

    prepare for errbit

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -20,6 +20,8 @@ docker pull redis
 docker pull nginx
 docker pull postgres
 docker pull skalera/influxdb
+docker pull mongodb
+docker pull griff/errbit
 
 # add influx command to VM image
 wget -O /usr/bin/influx https://s3-us-west-2.amazonaws.com/skalera/vagrant/influx-0.9.0_rc11
